19b4397c90a8d5e43d537ea8f1f3484dea984f3e
Richard Creedon <richardcreedon1989@gmail.com>
2020-09-22 20:25:52 +0100

    timeout for api requests added

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const items = [
+  { title: "What is React", content: "React is a frontend JS framework" },
+  {
+    title: "Why use React",
+    content: "Its a favourite JS library amongst Engineers",
+  },
+  { title: "How do you use React", content: "You use React by creating items" },
+];
+
+export default () => {
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
+    </div>
+  );
+};
